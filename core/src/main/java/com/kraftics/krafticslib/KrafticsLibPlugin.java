3e241afc3ce9326483518781309aa9e4359f5189
Panda885 <firegox@email.cz>
2021-06-11 10:54:41 +0200

    Added metrics

--- core/src/main/java/com/kraftics/krafticslib/KrafticsLibPlugin.java
+++ core/src/main/java/com/kraftics/krafticslib/KrafticsLibPlugin.java
@@ -1,6 +1,12 @@
 package com.kraftics.krafticslib;
 
+import com.kraftics.krafticslib.metrics.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KrafticsLibPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Metrics metrics = new Metrics(this, 9916);
+    }
 }
