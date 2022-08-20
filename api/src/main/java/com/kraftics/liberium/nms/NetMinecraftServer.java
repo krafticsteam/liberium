0619a5705af4c463bf334f552642ecc165101b7b
panda885 <firegox@email.cz>
2022-08-20 14:35:16 +0200

    Added NetMinecraftServer containing nms utilities

---
+++ api/src/main/java/com/kraftics/liberium/nms/NetMinecraftServer.java
@@ -0,0 +1,12 @@
+package com.kraftics.liberium.nms;
+
+import com.kraftics.liberium.command.CommandNodeConverter;
+import com.mojang.brigadier.CommandDispatcher;
+import org.bukkit.command.CommandMap;
+
+public interface NetMinecraftServer {
+
+    CommandMap getCommandMap();
+
+    CommandDispatcher<Object> getServerDispatcher();
+}
---
+++ api/src/main/java/com/kraftics/liberium/nms/NetMinecraftServerProvider.java
@@ -0,0 +1,12 @@
+package com.kraftics.liberium.nms;
+
+import org.bukkit.Server;
+import org.jetbrains.annotations.NotNull;
+
+public class NetMinecraftServerProvider {
+
+    @NotNull
+    public static NetMinecraftServer get(@NotNull Server server) {
+        throw new UnsupportedOperationException();
+    }
+}
