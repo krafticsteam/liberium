51dcf69a7b63a4ccb8200aaff4407125ef8d6cf6
panda885 <firegox@email.cz>
2022-08-20 15:15:22 +0200

    Expanded CommandManager with nms

--- api/src/main/java/com/kraftics/liberium/command/CommandManager.java
+++ api/src/main/java/com/kraftics/liberium/command/CommandManager.java
@@ -1,13 +1,29 @@
 package com.kraftics.liberium.command;
 
-import com.mojang.brigadier.CommandDispatcher;
-import org.bukkit.command.CommandSender;
+import com.kraftics.liberium.nms.NetMinecraftServer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class CommandManager {
-    private final CommandDispatcher<CommandSender> dispatcher = new CommandDispatcher<>();
+    private final List<LiberiumCommandNode> commands = new ArrayList<>();
+    private final NetMinecraftServer nms;
 
-    // TODO: Add checks for command already existing
+    public CommandManager(NetMinecraftServer nms) {
+        this.nms = nms;
+    }
+
     public void registerCommand(CommandArgumentBuilder command) {
-        this.dispatcher.register(command);
+        if (getCommand(command.getLiteral()).isPresent()) throw new IllegalArgumentException("A command with name " + command.getLiteral() + " is already registered");
+        LiberiumCommandNode node = command.build();
+        this.commands.add(node);
+        this.nms.getServerDispatcher().getRoot().addChild(this.nms.getCommandNodeConverter().convert(node));
+    }
+
+    @NotNull
+    public Optional<LiberiumCommandNode> getCommand(String name) {
+        return commands.stream().filter(node -> node.getName().equals(name)).findFirst(); // TODO: Check if commands are case-insensitive
     }
 }
