0263f4b43ca9c12f4a6ced8d3ca7addecb7455be
panda885 <firegox@email.cz>
2022-09-04 14:43:30 +0200

    Expanded plugin.yml with version property

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,3 +3,9 @@ dependencies {
     api(project(":database"))
     api(project(":packet"))
 }
+
+tasks.processResources {
+    filesMatching("plugin.yml") {
+        expand("version" to version)
+    }
+}
