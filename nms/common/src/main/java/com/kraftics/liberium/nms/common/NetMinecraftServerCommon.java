7aa2be5887fc00df87acb94aeaba511aee505d05
panda885 <firegox@email.cz>
2022-09-24 11:39:59 +0200

    Added nms creation from bukkit server

--- nms/common/src/main/java/com/kraftics/liberium/nms/common/NetMinecraftServerCommon.java
+++ nms/common/src/main/java/com/kraftics/liberium/nms/common/NetMinecraftServerCommon.java
@@ -2,10 +2,13 @@ package com.kraftics.liberium.nms.common;
 
 import com.kraftics.liberium.nms.NetMinecraftServer;
 import com.kraftics.liberium.nms.common.command.CommandNodeConverter;
+import com.kraftics.liberium.reflection.ObjectWrapper;
+import com.kraftics.liberium.reflection.exception.ReflectionException;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.tree.CommandNode;
 import net.minecraft.server.MinecraftServer;
+import org.bukkit.Server;
 import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 
@@ -14,6 +17,10 @@ public class NetMinecraftServerCommon implements NetMinecraftServer {
 
     public NetMinecraftServerCommon(MinecraftServer server) {
         this.server = server;
+    }
+
+    public static NetMinecraftServer fromBukkit(Server server) throws ReflectionException {
+        return new NetMinecraftServerCommon(ObjectWrapper.wrap(server).invoke("getServer").cast(MinecraftServer.class));
     }
 
     @Override
